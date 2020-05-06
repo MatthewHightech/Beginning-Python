16d1c7ae7931de3eb1e2ee4be4471a5536ad0fd5
MatthewHightech <Smoose007@gmail.com>
2020-05-06 16:22:20 -0700

    findSum

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+import numpy as np
+
+def findSum(matrix): 
+    sum = 0 
+    for row in range(0, 10):
+        for cell in range(0, 10):
+            sum += int(matrix[row][cell])
+    print(sum)
+
+matrix = np.random.random ([10,10]) * 10
+matrix.astype(int)
+
+findSum(matrix)
+        
 
